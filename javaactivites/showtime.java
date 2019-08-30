5d5ff68f5e47cf3f9071035b19568338e924fa2d
ColinVokt <voktcolin@gmail.com>
2019-08-30 09:19:50 -0600

    push to git

---
+++ javaactivites/showtime.java
@@ -0,0 +1,43 @@
+import java.util.Calendar;
+
+//Colin Vokt
+//8.30.19
+//Showtime
+	class ShowTime 
+{
+
+		public static void main(String[] args) 
+   {
+
+
+		System.out.println(Calendar.getInstance().getTime());
+			
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
+}
